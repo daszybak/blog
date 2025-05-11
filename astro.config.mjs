e92c9038100ec7d35ccc8bcb3b978fc0bd776969
daszybak <karlo1diidd@gmail.com>
2025-05-11 20:31:47 +0200

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.karlodelmis.com',
-	integrations: [mdx(), sitemap()],
-	output: "server"
+  site: 'https://blog.karlodelmis.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
